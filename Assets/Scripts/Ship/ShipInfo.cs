09fcafb34a6d224625ce728095c98130ad23d357
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-06-18 16:07:58 +0300

    Базовый пользователь

---
+++ Assets/Scripts/Ship/ShipInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame
+{
+	public class ShipInfo : MonoBehaviour
+	{
+		public readonly string m_name;
+
+	}
+}
---
+++ Assets/Scripts/User/User.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame
+{
+	public class User : MonoBehaviour
+	{
+		public string userName
+		{
+			get { return m_name; }
+			set { m_name = value; }
+		}
+
+		private string m_name = "IvanAndreyshev";
+
+		private uint m_experience = 300;
+		private ushort m_level = 16;
+		private ushort m_prestige = 0;
+
+		List<string> m_ships;
+		List<string> m_allowedShips;
+		List<string> m_achievements;
+	}
+}
